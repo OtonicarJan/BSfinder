5fe681849504436ba35f5063effa7114509ad942
jancl57 <78914975+jancl57@users.noreply.github.com>
2021-06-28 10:51:47 +0200

    Update BSfinder.py

--- BSfinder.py
+++ BSfinder.py
@@ -3,9 +3,10 @@ import argparse
 import numpy as np
 from Bio.Seq import Seq
 
-infile = argparse.ArgumentParser(description='Set input files and destination file')
-infile.add_argument('genome', metavar='genome', help='Set input csv depth file', type=argparse.FileType('r'))
-#set the width of the window
+infile = argparse.ArgumentParser(description='Set input files and destination file') 
+#set the input genome file
+infile.add_argument('genome', metavar='genome', help='Set the iniput genome', type=argparse.FileType('r'))
+#set file with list of binding sites
 infile.add_argument('binding_sites', metavar='list_of_binding_sites', help='file with aligned confirmed binding sites', type=argparse.FileType('r'))
 #set the output file
 infile.add_argument('dest_file', metavar='output_file', help='Set the name of your output file', type=argparse.FileType('w+'))
@@ -19,7 +20,7 @@ Motifs = []
 for line in motifs:
     Motifs.append(line.strip())
 
-
+#search window by window thorugh genome 
 def Search(Motifs, Genome):
     tem = Genome.readlines()[1:]  
     genome = ""
@@ -41,6 +42,7 @@ def Search(Motifs, Genome):
             bindingsites[i+1, i+k+1] = scoreReverse
     return bindingsites
 
+#calculate the top 20% of hits based on scoring
 def percentile(Motifs, Genome):
     profil = profile(Motifs)
     #Rseq = RSequence(Motifs, Genome)
@@ -60,6 +62,7 @@ def percentile(Motifs, Genome):
     osem = np.percentile(listofscores, 20)
     return osem
 
+#scoring function based on the calculated profile
 def BvH(pattern, Motifs):
     profil = profile(Motifs)
     #Rseq = RSequence(Motifs, Genome)
@@ -78,6 +81,7 @@ def BvH(pattern, Motifs):
     #osem = np.percentile(listofscores, 20)
     return BvH
 
+#calculate the profile from aligned binding sites
 def profile(Motifs):
     profile = Count(Motifs)
     t = len(Motifs)
@@ -87,21 +91,7 @@ def profile(Motifs):
 
     return profile
 
-def Consensus(Motifs):
-    counting = Count(Motifs)
-    #t = len(alignedmotifs)
-    k = len(Motifs[0])
-    consensus = ""
-    for i in range(k):
-        m = 0
-        frequentSymbol = ""
-        for letter in "ACGT":
-            if counting[letter][i] > m:
-                m = counting[letter][i]
-                frequentSymbol = letter
-        consensus += frequentSymbol
-    return consensus
-
+#count number of appearances of each letter at each position
 def Count(Motifs):
     #for line in motifs:
         #alignedmotifs.append(line.strip())
